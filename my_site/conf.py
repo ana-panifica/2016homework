aaca9c85b25c51569b417317e4c4089a7a43ee8d
Anastasia Panova <anastasia.b.panova@gmail.com>
2017-05-11 17:43:17 +0300

    Delete conf.py

--- my_site/conf.py
+++
@@ -1,5 +0,0 @@
-TOKEN = "366679638:AAEetzkwUAnbPDy-cIRjN1MHNBbSS07S6Yk"
-WEBHOOK_HOST = 'anapanifica.pythonanywhere.com'
-WEBHOOK_PORT = '443'
-# телеграм может работать с портами 443, 80, 88 или 8443
-# при этом pythonanywhere разрешает использовать только порт 443 - стандартный https порт
